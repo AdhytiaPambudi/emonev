bc3306e86bfc5b4aef99ddbe759aedca17ca3c1b
Dhea Aria Rachman <dheariarachman@gmail.com>
2020-10-18 23:21:50 +0000

    Initial with gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -31,6 +31,7 @@ error_reporting(0);
 $base  = "http://".$_SERVER['HTTP_HOST'];
 $base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $base;
+// $config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
